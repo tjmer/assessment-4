38cb73043c5f02083be6ed3e0e7fa62863111fa8
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-13 12:17:32 -0600

    Added most of the extra credit

--- sql-skills.sql
+++ sql-skills.sql
@@ -47,3 +47,40 @@ JOIN employee e ON e.employee_id = c.support_rep_id;
 
 SELECT a.title, ar.name FROM album a
 JOIN artist ar ON ar.artist_id = a.artist_id;
+
+-- EXTRA CREDIT
+-- Artist table
+
+SELECT name FROM artist
+ORDER BY name DESC LIMIT 10;
+
+SELECT * FROM artist
+WHERE name LIKE 'Black%';
+
+SELECT * FROM artist
+WHERE name LIKE '%Black%';
+
+-- Employee Table
+
+SELECT MAX(birth_date) FROM employee;
+
+SELECT MIN(birth_date) FROM employee;
+
+-- Invoice Table
+
+SELECT COUNT(invoice_id) FROM invoice
+WHERE billing_state IN ('CA') OR billing_state IN ('TX') OR billing_state IN ('AZ');
+
+SELECT AVG(total) FROM invoice;
+
+-- More Join Queries
+
+SELECT track_id FROM playlist_track
+WHERE track_id IN(
+  SELECT track_id FROM playlist
+  WHERE name = 'Music');
+
+SELECT name FROM track
+WHERE track_id IN(
+  SELECT track_id FROM playlist_track
+  WHERE playlist_id = 5);
